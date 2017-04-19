84361c88993494694b196663afaae61b3d304b5f
Emil Wahlund <emilwahlund96@gmail.com>
2017-04-19 11:05:45 +0200

    Good Gitignore

---
+++ Source/GitTest/V1.cpp
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GitTest.h"
+#include "V1.h"
+
+
+// Sets default values
+AV1::AV1()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AV1::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AV1::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AV1::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
